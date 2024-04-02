657b1339c98ed6a63670dd2301d464d7876ff83f
brian <bapipi@prateducacio.com>
2024-04-02 18:08:37 +0200

    probando

--- lib/main.dart
+++ lib/main.dart
@@ -107,7 +107,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'probado enlaze  brian git:',
+              'probado enlaze  brian  ffgit:',
             ),
             Text(
               '$_counter',
