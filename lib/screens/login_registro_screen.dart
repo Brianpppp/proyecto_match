be29460a82ed23344e1c9f926837c666e0b9d338
Joan Atrio <atriopozojoan@gmail.com>
2024-04-08 17:21:16 +0200

    Logo in login/Register page + visual changes

--- lib/screens/login_registro_screen.dart
+++ lib/screens/login_registro_screen.dart
@@ -19,8 +19,10 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Authentication',
       theme: ThemeData(
         primarySwatch: Colors.pink,
-        colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.pink), // Definir el esquema de colores con el tono de rosa
-
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          secondary: Colors.pink,
+          background: Color.fromRGBO(255, 169, 209, 1.0), // Establecer el color de fondo de la aplicación
+        ),
       ),
       home: AuthScreen(),
     );
@@ -102,10 +104,11 @@ class _AuthScreenState extends State<AuthScreen> {
         SnackBar(
           content: Text('Password reset email sent!'),
           duration: Duration(seconds: 2),
+
         ),
       );
     } catch (error) {
-      _showErrorDialog('Failed to send reset email. Please check your email address.');
+      _showErrorDialog('Failed to send reset password. Please check your email address.');
     }
   }
 
@@ -130,130 +133,146 @@ class _AuthScreenState extends State<AuthScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromRGBO(255, 169, 209, 1.0), // Color de fondo rosa
+
       appBar: AppBar(
         title: Text(_isLogin ? 'Log in' : 'Sign Up'),
       ),
-      body: Container(
-        color: Colors.pink[100],
-        padding: EdgeInsets.all(20),
-        child: Center(
-          child: SingleChildScrollView(
-            child: Card(
-              elevation: 5,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30),
-              ),
-              child: Padding(
-                padding: EdgeInsets.all(20),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              _isLogin = true;
-                            });
-                          },
-                          child: Container(
-                            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                            decoration: BoxDecoration(
-                              color: _isLogin ? Colors.pinkAccent : Colors.grey,
-                              borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(30),
-                                bottomLeft: Radius.circular(30),
-                              ),
-                            ),
-                            child: Text(
-                              'Log in',
-                              style: TextStyle(color: Colors.white, fontSize: 18),
-                            ),
-                          ),
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              _isLogin = false;
-                            });
-                          },
-                          child: Container(
-                            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                            decoration: BoxDecoration(
-                              color: _isLogin ? Colors.grey : Colors.pinkAccent,
-                              borderRadius: BorderRadius.only(
-                                topRight: Radius.circular(30),
-                                bottomRight: Radius.circular(30),
-                              ),
-                            ),
-                            child: Text(
-                              'Sign Up',
-                              style: TextStyle(color: Colors.white, fontSize: 18),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 20),
-                    Text(
-                      _isLogin
-                          ? 'Welcome to MATCH!\nLog in here'
-                          : 'Welcome to MATCH!\nSign up here',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(color: Colors.black, fontSize: 20),
-                    ),
-                    SizedBox(height: 10),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 20),
-                      child: TextField(
-                        controller: _emailController,
-                        decoration: InputDecoration(
-                          labelText: 'Email',
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
-                        ),
-                        keyboardType: TextInputType.emailAddress,
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 20),
-                      child: TextField(
-                        controller: _passwordController,
-                        decoration: InputDecoration(
-                          labelText: 'Password',
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
-                        ),
-                        obscureText: true,
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    ElevatedButton(
-                      onPressed: () => _authenticate(context),
-                      style: ElevatedButton.styleFrom(
-                        textStyle: TextStyle(fontSize: 18),
-                      ),
-                      child: Text(_isLogin ? 'Log in' : 'Sign Up'),
-                    ),
-                    SizedBox(height: 10),
-                    if (_isLogin)
-                      TextButton(
-                        onPressed: _resetPassword,
-                        child: Text(
-                          'Forgot Password?',
-                          style: TextStyle(fontSize: 16, color: Colors.pink),
-                        ),
-                      ),
-                  ],
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                color: Color.fromRGBO(255, 169, 209, 1.0),
+                height: 100, // Ajusta según tus necesidades
+                width: 200,
+                child: Image.asset(
+                  'assets/match2.png',
+                  fit: BoxFit.cover,
                 ),
               ),
-            ),
+              SizedBox(height: 20),
+              SizedBox(
+                width: 300, // Anchura deseada para la tarjeta
+                child: Card(
+                  elevation: 5,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(40),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  _isLogin = true;
+                                });
+                              },
+                              child: Container(
+                                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                                decoration: BoxDecoration(
+                                  color: _isLogin ? Colors.pinkAccent : Colors.grey,
+                                  borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(30),
+                                    bottomLeft: Radius.circular(30),
+                                  ),
+                                ),
+                                child: Text(
+                                  'Log in',
+                                  style: TextStyle(color: Colors.white, fontSize: 18),
+                                ),
+                              ),
+                            ),
+                            GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  _isLogin = false;
+                                });
+                              },
+                              child: Container(
+                                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                                decoration: BoxDecoration(
+                                  color: _isLogin ? Colors.grey : Colors.pinkAccent,
+                                  borderRadius: BorderRadius.only(
+                                    topRight: Radius.circular(30),
+                                    bottomRight: Radius.circular(30),
+                                  ),
+                                ),
+                                child: Text(
+                                  'Sign Up',
+                                  style: TextStyle(color: Colors.white, fontSize: 18),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 20),
+                        Text(
+                          _isLogin
+                              ? 'Welcome to MATCH!\nLog in here'
+                              : 'Welcome to MATCH!\nSign up here',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(color: Colors.black, fontSize: 20),
+                        ),
+                        SizedBox(height: 10),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 10),
+                          child: TextField(
+                            controller: _emailController,
+                            decoration: InputDecoration(
+                              labelText: 'Email',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                            ),
+                            keyboardType: TextInputType.emailAddress,
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 10),
+                          child: TextField(
+                            controller: _passwordController,
+                            decoration: InputDecoration(
+                              labelText: 'Password',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                            ),
+                            obscureText: true,
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        ElevatedButton(
+                          onPressed: () => _authenticate(context),
+                          style: ElevatedButton.styleFrom(
+                            textStyle: TextStyle(fontSize: 18),
+                          ),
+                          child: Text(_isLogin ? 'Log in' : 'Sign Up'),
+                        ),
+                        SizedBox(height: 10),
+                        if (_isLogin)
+                          TextButton(
+                            onPressed: _resetPassword,
+                            child: Text(
+                              'Forgot Password?',
+                              style: TextStyle(fontSize: 16, color: Colors.pink),
+                            ),
+                          ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
