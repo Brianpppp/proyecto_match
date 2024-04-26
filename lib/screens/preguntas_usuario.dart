570a961e05ea307b7daadc41662500271bcf1686
Joan Atrio <atriopozojoan@gmail.com>
2024-04-26 15:20:46 +0200

    import no usado

--- lib/screens/preguntas_usuario.dart
+++ lib/screens/preguntas_usuario.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'preguntas_usuario2.dart';
-import 'login_registro_screen.dart';
 
 class PreguntasUsuario extends StatefulWidget {
   final String email;
